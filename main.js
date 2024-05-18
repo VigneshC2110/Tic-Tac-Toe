851cb1dccf935acc85af628198d7603bc6fb4cf4
“vigneshcdckap” <vigneshcdckap@gmail.com>
2024-05-18 16:56:17 +0530

    game

---
+++ main.js
@@ -0,0 +1,70 @@
+let statusDisplay = document.querySelector('.status');
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+let winningConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6]];
+
+let updateStatusMessage = (message, color = "rgb(65, 65, 65)") => {
+    statusDisplay.innerHTML = message;
+    statusDisplay.style.color = color;
+};
+
+updateStatusMessage(`It's ${currentPlayer}'s turn`);
+
+function playCell(cell, index) {
+    gameState[index] = currentPlayer;
+    cell.innerHTML = currentPlayer;
+}
+
+function switchPlayer() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    updateStatusMessage(`It's ${currentPlayer}'s turn`);
+}
+
+function validateGameResult() {
+    let roundWon = false;
+
+    for (let i = 0; i < winningConditions.length; i++) {
+        let [a, b, c] = winningConditions[i];
+        if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        updateStatusMessage(`Player ${currentPlayer} has won!`, "rgb(251,100,204)");
+        gameActive = false;
+        return;
+    }
+
+    if (!gameState.includes("")) {
+        updateStatusMessage("Game ended in a draw!", "rgb(251,100,204)");
+        gameActive = false;
+        return;
+    }
+
+    switchPlayer();
+}
+
+function processCellClick(event) {
+    let cell = event.target;
+    let index = parseInt(cell.getAttribute('data-cell-index'));
+
+    if (gameState[index] || !gameActive) {
+        return;
+    }
+    playCell(cell, index);
+    validateGameResult();
+}
+
+function restartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    updateStatusMessage(`It's ${currentPlayer}'s turn`);
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', processCellClick));
+document.querySelector('.restart').addEventListener('click', restartGame);
